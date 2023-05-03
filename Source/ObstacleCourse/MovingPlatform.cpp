7cd4920cb210e1ae96090f40c83a10e1714ef437
Number5IsAlive <james.s.mason@hotmail.com>
2023-05-03 14:20:17 +0100

    20 Scope Resolution Operator

--- Source/ObstacleCourse/MovingPlatform.cpp
+++ Source/ObstacleCourse/MovingPlatform.cpp
@@ -16,6 +16,7 @@ void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
 
+	StartLocation = GetActorLocation();
 }
 
 // Called every frame
@@ -32,6 +33,7 @@ void AMovingPlatform::Tick(float DeltaTime)
 	SetActorLocation(CurrentLocation);
 	// Send platform back if gone too far
 		// Check how far platform has moved
+	MoveDistance = FVector::Dist(StartLocation, CurrentLocation);
 		// Reverse direction if gone too far
 }
 
